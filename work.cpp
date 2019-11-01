8760ea83551db0d003aea250b5981e1a3fa34a64
DK803 <57218774+DK803@users.noreply.github.com>
2019-11-02 00:25:53 +0900

    Add files via upload

---
+++ work.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int total, tshirt, sweater; // 필요한 변수를 3개 선언
+
+	cout << ("티셔츠의 값을 입력 하시오 : ", tshirt);// 옷 값 입력
+	cin >> (tshirt); // 여기 받는 부분
+
+	if (total <= 10000)
+		(tshirt + sweater)*(5 %);
+
+	else
+		(tshirt + sweater)*(15 % );
+
+	return 0;
+}
